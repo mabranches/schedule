7d65af8c3e5bf1d8e1deaa6a172547d1da366ebf
Manoel Rebelo Abranches <abranches@gmail.com>
2016-08-24 14:36:41 -0300

    model validations

--- app/models/scheduling.rb
+++ app/models/scheduling.rb
@@ -1,3 +1,6 @@
 class Scheduling < ApplicationRecord
   belongs_to :user
+  validates :day, presence: true
+  validates :hour, presence: true, uniqueness: {scope: [:day]}
+  validates :user, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable
-  has_many :Schedulings
+  has_many :schedulings, dependent: :destroy
+  validates :name, presence: true
+  validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
 end
