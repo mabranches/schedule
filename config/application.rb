76646448f35e1d25a4768d8942c8c9ee7a36493b
Manoel Rebelo Abranches <abranches@gmail.com>
2016-08-24 02:03:23 -0300

    config for start and end time.

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,9 @@ module Schedule
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.autoload_paths << "#{Rails.root}/lib"
+    config.eager_load_paths << "#{Rails.root}/lib"
+    config.autoload_paths << "#{Rails.root}/lib/renderers"
+    config.eager_load_paths << "#{Rails.root}/lib/renderers"
   end
 end
