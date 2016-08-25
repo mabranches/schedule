7d38c5a793a515d8eaab9e802e431d8b362320d2
Manoel Rebelo Abranches <abranches@gmail.com>
2016-08-25 05:09:54 -0300

    add event after scheduling_cell changes

--- app/assets/javascripts/schedulings.js
+++ app/assets/javascripts/schedulings.js
@@ -1,4 +1,3 @@
-$(window).load(function(e) {
   function cancelchedulingClick(ev){
     var scheduling_cell = $(this).closest('.scheduling')
     var scheduling_id = parseInt(scheduling_cell.attr("data-scheduling-id"));
@@ -13,37 +12,26 @@ $(window).load(function(e) {
     createScheduling({scheduling : {day: day , hour: hour} }, this);
   }
 
-
- loadButtonLogic();
-  function loadButtonLogic(){
-    //function that schedule a room
-    $("#tbl-selectRoom tbody td .btn.btn-success").off('click');
-    $("#tbl-selectRoom tbody td .badge").off('click');
-    $("#tbl-selectRoom tbody td .btn.btn-success").click(schedulingClick);
-
-     //function that unschedule a room
-    $("#tbl-selectRoom tbody td .badge").click(cancelchedulingClick);
-    function createScheduling(scheduling, btn){
+  function createScheduling(scheduling, btn){
       var scheduling_cell = $(btn).closest('.scheduling')
-        debugger;
-      scheduleRoomService( this,'POST','schedulings', function(msg){
+      scheduleRoomService(btn,'POST','schedulings', function(msg){
         scheduling_cell.attr("data-scheduling-id",msg["scheduling-id"]);
         scheduling_cell.append("<div class='cancel'><span class='badge'>x</span></div>");
         scheduling_cell.append("<span class='userName'>" + $("#user-name").text().trim() + "</span>");
         scheduling_cell.removeClass("available").addClass("busy").find("button").remove();
-        loadButtonLogic();
+        scheduling_cell.find('.badge').click(cancelchedulingClick)
       }, scheduling);
     }
 
     function cancelScheduling(btn,scheduling_id){
       var scheduling_cell = $(btn).closest('.scheduling')
-      scheduleRoomService(this,'DELETE','schedulings/' + scheduling_id, function(msg){
+      scheduleRoomService(btn,'DELETE','schedulings/' + scheduling_id, function(msg){
         scheduling_cell.removeAttr("data-scheduling-id");
         scheduling_cell.removeClass("busy").addClass("available");
         scheduling_cell.find(".userName").remove();
         scheduling_cell.append("<button type='button' class='btn btn-success'>Agendar</button>");
         scheduling_cell.find('.cancel').remove();
-        loadButtonLogic();
+        scheduling_cell.find('.btn-success').click(schedulingClick);
       });
     }
 
@@ -53,7 +41,6 @@ $(window).load(function(e) {
         url: url,
         data: scheduling,
         success: function(msg){
-          $(btn).prop('disabled', false);
           callback(msg);
         },
         error: function(msg){
@@ -61,5 +48,9 @@ $(window).load(function(e) {
         }
       });
     }
-  }
+
+$(window).load(function(e) {
+  $("#tbl-selectRoom tbody td .btn.btn-success").click(schedulingClick);
+  $("#tbl-selectRoom tbody td .badge").click(cancelchedulingClick);
+
 });
