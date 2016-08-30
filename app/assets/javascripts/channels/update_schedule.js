5033b31193fb0aedde0c96c4c6a47ffb392d96f8
Manoel Rebelo Abranches <abranches@gmail.com>
2016-08-30 12:35:46 -0300

    refactory receiving websocket methods in js

--- app/assets/javascripts/channels/update_schedule.js
+++ app/assets/javascripts/channels/update_schedule.js
@@ -6,17 +6,17 @@ function same_user(data){
 function receive_common(data, receive_function){
   if (same_user(data))
     return;
-  receive_function();
+  schdedulingCellId = '#' + data['day'] +
+                        '-' +data['hour'];
+  schedulingCell = $(schdedulingCellId);
+  receive_function(schedulingCell);
   return
 }
 
 CreateChannel.messages = CreateChannel.cable.
   subscriptions.create('CreatedSchedulingChannel', {
   received: function(data) {
-    receive_common(data, function(){
-      schdedulingCellId = '#' + data['scheduling']['day'] +
-                            '-' +data['scheduling']['hour'];
-      schedulingCell = $(schdedulingCellId);
+    receive_common(data, function(schedulingCell){
       schedulingCell.html("<span class='userName'>" +
                             data['user']['name'] + "</span>");
     });
@@ -26,10 +26,7 @@ CreateChannel.messages = CreateChannel.cable.
 CancelChannel.messages = CancelChannel.cable.
   subscriptions.create('CanceledSchedulingChannel', {
   received: function(data) {
-    receive_common(data, function(){
-      schdedulingCellId = '#' + data['scheduling']['day'] +
-                            '-' +data['scheduling']['hour'];
-      schedulingCell = $(schdedulingCellId);
+    receive_common(data, function(schedulingCell){
       emptyScheduling = new EmptyScheduling(schedulingCell);
       emptyScheduling.build();
     })
